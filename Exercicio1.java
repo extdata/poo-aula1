49fb84d80291603a2876f18dd0d121ffd474d308
Ricardo <rjmaciel90@gmail.com>
2018-03-26 21:16:39 -0300

    Aula Java

---
+++ Exercicio1.java
@@ -0,0 +1,14 @@
+package aula1;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Quantos anos você tem?");
+		int numero = scanner.nextInt();
+		
+		//verifica a idade, se for maior que 30 imprime "Você é velho!"
+		System.out.println();
+	}
+}
---
+++ Exercicio2.java
@@ -0,0 +1,19 @@
+package aula1;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+	public static void main( String[] args ) {
+		Scanner scanner = new Scanner( System.in );
+		System.out.println( "Quantos anos você tem?" );
+		int numero = scanner.nextInt();
+			if ( numero > 30 ) {
+				System.out.println( "Tá velho" );
+			} else if ( numero < 15 ) {
+					System.out.println("");
+			}
+		
+		//verifica a idade, se for maior que 30 imprime "Você é velho!"
+		System.out.println();
+	}
+}
